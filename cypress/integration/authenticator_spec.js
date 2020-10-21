5ba647d861109463f0f26bda37b10601de1cb312
paularintaharri <paula.rintaharri@gmail.com>
2020-10-21 18:34:45 +0300

    added cypress test

--- cypress/integration/authenticator_spec.js
+++ cypress/integration/authenticator_spec.js
@@ -1,26 +1,39 @@
   describe('Authenticator:', function() {
     // Step 1: setup the application state
     beforeEach(function() {
-      cy.visit('http://localhost:3000/');
+      cy.visit('/');
     });
+
+    const USERNAME = "user12345";
+    const PASSWORD = "user12345";
     
     describe('Sign In:', () => {
     it('allows a user to signin', () => {
+      
         // Step 2: Take an action (Sign in)
-       
-        cy.frameLoaded()
-        cy.get(selectors.signInSignInButton).type("user12345");
-            //cy.get(selectors.signInPasswordInput).type("user12345");
-            //cy.get(selectors.signInSignInButton).contains('Sign In').click();
-      
-            // Step 3: Make an assertion (Check for sign-out text)
-            // cy.get(selectors.signOutButton).contains('Sign Out');
+        cy.get("amplify-authenticator").find(selectors.signInusernameInput, {
+          includeShadowDom: true,
+        })
+        .type(USERNAME);
 
-         
-        });
-        });
-      
+        cy.get("amplify-authenticator").find(selectors.signInPasswordInput, {
+          includeShadowDom: true,
+        })
+        .type(PASSWORD, { force: true });
+        
+        cy.get("amplify-authenticator").find(selectors.signInSignInButton, {
+          includeShadowDom: true,
+        })
+        .first()
+        .find("button[type='submit']", { includeShadowDom: true })
+        .click({ force: true });
+
+      // Step 3: Make an assertion (Check for sign-out text)
+      cy.get("amplify-sign-out").find(selectors.signOutButton, { includeShadowDom: true })
+        .contains("Sign Out");
+    });
   });
+});
  export const selectors = {
     // Auth component classes
     signInusernameInput: '[data-test="sign-in-username-input"]',
